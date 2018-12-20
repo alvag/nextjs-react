1d89c30fc4666ec10fa69b3d597afadf8872896d
Max Alva <alva85@gmail.com>
2018-12-19 22:14:13 -0500

    Mostrando cada noticia

--- components/modules/News/News.tsx
+++ components/modules/News/News.tsx
@@ -7,8 +7,25 @@ interface IProps {
 
 const News = (props: IProps) => {
     return (
-        <div>
-
+        <div className="row">
+            {props.news.map((item, index) => (
+                <div key={index} className="col-md-6 col-12 mb-4">
+                    <div className="card">
+                        <div className="card-image">
+                            {item.urlToImage !== ''
+                                ? <img className="card-image-top" src={item.urlToImage} alt={item.title} />
+                                : <p className="text-center my-5">Imagen no disponible</p>
+                            }
+                        </div>
+                        <div className="card-body">
+                            <h3 className="card-title">{item.title}</h3>
+                            <p className="card-text">{item.description}</p>
+                            <div className="card-text">{item.source.name}</div>
+                            <a href={item.url} className="btn btn-primary d-block">Leer noticia</a>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
